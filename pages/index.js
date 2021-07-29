bf4fc5ad04bc55a0eb304b8a6aaa4be7f79d42a0
cesarlp81 <cesarlp81@gmail.com>
2021-07-28 23:07:50 -0300

    original

--- pages/index.js
+++ pages/index.js
@@ -226,7 +226,7 @@ export default function Home(props) {
 export async function getServerSideProps(context) {
   const cookies = nookies.get(context)
   const token = cookies.USER_TOKEN;
-  const { isAuthenticated } = await fetch('alurakut-cesarlp81.vercel.app/api/auth', {
+  const { isAuthenticated } = await fetch('https://alurakut.vercel.app/api/auth', {
     headers: {
         Authorization: token
       }
